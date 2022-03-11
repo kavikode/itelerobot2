6c41d46c02c38a6572ce27338a4fd251fc2748a0
Kavi <kavi1@umbc.edu>
2022-03-11 02:25:05 +0000

    add files commit

---
+++ devel/share/dodo_detector_ros/cmake/dodo_detector_ros-msg-paths.cmake
@@ -0,0 +1,4 @@
+# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
+
+set(dodo_detector_ros_MSG_INCLUDE_DIRS "/home/kavihost/catkin_ws/src/simulation_ws/src/perception_object_detection/dodo_detector_ros/msg")
+set(dodo_detector_ros_MSG_DEPENDENCIES std_msgs;geometry_msgs)
---
+++ devel/share/gennodejs/ros/grasping_msgs/msg/_index.js
@@ -0,0 +1,40 @@
+
+"use strict";
+
+let GraspPlanningAction = require('./GraspPlanningAction.js');
+let FindGraspableObjectsActionGoal = require('./FindGraspableObjectsActionGoal.js');
+let FindGraspableObjectsActionResult = require('./FindGraspableObjectsActionResult.js');
+let FindGraspableObjectsResult = require('./FindGraspableObjectsResult.js');
+let GraspPlanningActionResult = require('./GraspPlanningActionResult.js');
+let GraspPlanningGoal = require('./GraspPlanningGoal.js');
+let FindGraspableObjectsActionFeedback = require('./FindGraspableObjectsActionFeedback.js');
+let FindGraspableObjectsFeedback = require('./FindGraspableObjectsFeedback.js');
+let GraspPlanningFeedback = require('./GraspPlanningFeedback.js');
+let GraspPlanningActionGoal = require('./GraspPlanningActionGoal.js');
+let GraspPlanningActionFeedback = require('./GraspPlanningActionFeedback.js');
+let GraspPlanningResult = require('./GraspPlanningResult.js');
+let FindGraspableObjectsAction = require('./FindGraspableObjectsAction.js');
+let FindGraspableObjectsGoal = require('./FindGraspableObjectsGoal.js');
+let Object = require('./Object.js');
+let GraspableObject = require('./GraspableObject.js');
+let ObjectProperty = require('./ObjectProperty.js');
+
+module.exports = {
+  GraspPlanningAction: GraspPlanningAction,
+  FindGraspableObjectsActionGoal: FindGraspableObjectsActionGoal,
+  FindGraspableObjectsActionResult: FindGraspableObjectsActionResult,
+  FindGraspableObjectsResult: FindGraspableObjectsResult,
+  GraspPlanningActionResult: GraspPlanningActionResult,
+  GraspPlanningGoal: GraspPlanningGoal,
+  FindGraspableObjectsActionFeedback: FindGraspableObjectsActionFeedback,
+  FindGraspableObjectsFeedback: FindGraspableObjectsFeedback,
+  GraspPlanningFeedback: GraspPlanningFeedback,
+  GraspPlanningActionGoal: GraspPlanningActionGoal,
+  GraspPlanningActionFeedback: GraspPlanningActionFeedback,
+  GraspPlanningResult: GraspPlanningResult,
+  FindGraspableObjectsAction: FindGraspableObjectsAction,
+  FindGraspableObjectsGoal: FindGraspableObjectsGoal,
+  Object: Object,
+  GraspableObject: GraspableObject,
+  ObjectProperty: ObjectProperty,
+};
